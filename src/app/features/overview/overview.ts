34b7b6c4f61a48d29612d75b57d5853c2d15e185
yohagos <yosef.hagos@googlemail.com>
2025-11-08 21:12:07 +0100

    added overview component as landing page and overview of functions

---
+++ src/app/features/overview/overview.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { MatDividerModule } from '@angular/material/divider';
+
+@Component({
+  selector: 'app-overview',
+  imports: [
+    MatDividerModule,
+  ],
+  templateUrl: './overview.html',
+  styleUrl: './overview.scss',
+})
+export class Overview {
+
+}
