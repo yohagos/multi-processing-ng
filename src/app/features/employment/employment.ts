8de35444de5f32ab67d2b8f026df7030dd614f84
yohagos <yosef.hagos@googlemail.com>
2025-11-18 11:48:41 +0100

    feat: added employment component as spezific overview component for users, departments, etc

---
+++ src/app/features/employment/employment.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-employment',
+  imports: [
+     
+  ],
+  templateUrl: './employment.html',
+  styleUrl: './employment.scss',
+})
+export class Employment {
+
+}
