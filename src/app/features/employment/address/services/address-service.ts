fbb9112871e953ac879ec3ff2a1bbd5412e3b3be
yohagos <yosef.hagos@googlemail.com>
2025-11-25 17:11:30 +0100

    feat: added service for address

---
+++ src/app/features/employment/address/services/address-service.ts
@@ -0,0 +1,12 @@
+import { inject, Injectable } from '@angular/core';
+import { AddressAdapter } from './address-adapter';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AddressService {
+  private addressAdapterService = inject(AddressAdapter)
+  private httpClient = inject(HttpClient)
+  private baseUrl = 'http://localhost:8080/api/address'
+}
