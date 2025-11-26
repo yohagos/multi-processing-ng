fa6d3dbff16907176059f5ad0c5535f72cccfe75
yohagos <yosef.hagos@googlemail.com>
2025-11-26 18:55:28 +0100

    fix: clean up comments from user adapter service

--- src/app/features/employment/users/services/user-adapter.ts
+++ src/app/features/employment/users/services/user-adapter.ts
@@ -14,47 +14,6 @@ export class UserAdapter {
   private _addressAdapter = inject(AddressAdapter)
   private _skillAdapter = inject(SkillAdapter)
 
-  /* toUi(data: UserApiData): UserUiData {
-    return {
-      id: data.id,
-      email: data.email,
-      first_name: data.first_name,
-      last_name: data.last_name,
-      full_name: `${data.first_name} ${data.last_name}`,
-      department_id: data.department_id,
-      position_id: data.position_id,
-      hire_date: data.hire_date,
-      phone: data.phone,
-      date_of_birth: data.date_of_birth,
-      created_at: data.created_at,
-      updated_at: data.updated_at,
-    };
-  }
-
-  toApi(data: UserUiData): UserApiData {
-    return {
-      id: data.id,
-      email: data.email,
-      first_name: data.first_name,
-      last_name: data.last_name,
-      department_id: data.department_id,
-      position_id: data.position_id,
-      hire_date: data.hire_date,
-      phone: data.phone,
-      date_of_birth: data.date_of_birth,
-      created_at: data.created_at,
-      updated_at: data.updated_at,
-    };
-  }
-
-  toUserUiList(apiList: UserApiData[]): UserUiData[] {
-    return apiList.map((data) => this.toUi(data));
-  }
-
-  toUserApiList(uiList: UserUiData[]): UserApiData[] {
-    return uiList.map((data) => this.toApi(data));
-  } */
-
   toUiDataWithDetails(data: UserApiDataWithDetails): UserUiDataWithDetails {
     return {
       id: data.id,
