31b3bda778b1a826c244e058a617f6065d587e3c
yohagos <yosef.hagos@googlemail.com>
2025-11-28 17:02:20 +0100

    add: adding and removing skill based on skill_id and user_id

--- src/app/features/employment/skill/services/skill-service.ts
+++ src/app/features/employment/skill/services/skill-service.ts
@@ -1,9 +1,10 @@
 import { inject, Injectable, signal, WritableSignal } from '@angular/core';
 import { SkillAdapter } from './skill-adapter';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 
-import { catchError, of } from 'rxjs';
+import { catchError, of, tap } from 'rxjs';
 import { SkillPage, SkillWithDetailsApiData, SkillWithDetailsUiData } from '../../models/skill.model';
+import { UserService } from '../../users/services/user-service';
 
 @Injectable({
   providedIn: 'root'
@@ -12,6 +13,8 @@ export class SkillService {
   private skillAdapterService = inject(SkillAdapter)
   private httpClient = inject(HttpClient)
   private baseUrl = "http://localhost:8080/api/skill"
+
+  private userService = inject(UserService)
 
   public skillsByUserId: WritableSignal<SkillWithDetailsUiData[] | null> = signal(null)
   skillList: WritableSignal<SkillPage> = signal({
@@ -44,6 +47,35 @@ export class SkillService {
         error: result.error
       })
     })
+  }
+
+  addSkillForUser(
+    skill_id: string,
+    user_id: string,
+  ) {
+    this.httpClient.post(`${this.baseUrl}/add/${user_id}/skill/${skill_id}`,{}).pipe(
+      tap(() => {
+        this.userService.getUsers(1, 10)
+      }),
+      catchError((err) => {
+        console.error(err)
+        return of(err)
+      })
+    ).subscribe()
+  }
+
+  removeSkillForUser(
+    skill_id: string,
+    user_id: string,
+  ) {
+    this.httpClient.delete(`${this.baseUrl}/delete/${user_id}/skill/${skill_id}`).pipe(
+      tap(() => {
+        this.userService.getUsers(1, 10)
+      }),
+      catchError((err) => {
+        return of(err)
+      })
+    ).subscribe()
   }
 
 }
