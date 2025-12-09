a4190bdf49c2355bcb85588c6c374488fd084ca5
yohagos <yosef.hagos@googlemail.com>
2025-12-09 21:41:54 +0100

    fix: adding provideNtiveDateAdapter for using MatDatePicker

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,6 +3,7 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
+import { provideNativeDateAdapter } from '@angular/material/core';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -10,5 +11,6 @@ export const appConfig: ApplicationConfig = {
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideHttpClient(),
+    provideNativeDateAdapter(),
   ]
 };
