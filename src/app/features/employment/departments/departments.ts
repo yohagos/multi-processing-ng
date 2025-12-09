acbd90d806ff2fc13a33f7806d55fc1d0bd195ed
yohagos <yosef.hagos@googlemail.com>
2025-12-09 21:46:03 +0100

    fix: loading and displaying departments

--- src/app/features/employment/departments/departments.ts
+++ src/app/features/employment/departments/departments.ts
@@ -1,11 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnDestroy } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { DepartmentService } from './services/department-service';
+import { toObservable } from '@angular/core/rxjs-interop';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { DepartmentCard } from './department-card/department-card';
+import { DepartmentUiData } from '../models/department.model';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-departments',
-  imports: [],
+  imports: [
+    MatButtonModule,
+    MatIconModule,
+    MatProgressSpinnerModule,
+    MatProgressBarModule,
+
+    DepartmentCard,
+  ],
   templateUrl: './departments.html',
   styleUrl: './departments.scss',
 })
-export class Departments {
+export class Departments implements OnDestroy {
+  private departmentService = inject(DepartmentService)
+  private router = inject(Router)
 
+  loading = true
+
+  departments = this.departmentService.departmentList
+
+  constructor() {
+    toObservable(this.departments).subscribe(data => {
+      if (data.data) {
+        this.loading = false
+      }
+    })
+    this.departmentService.loadDepatments()
+  }
+
+  openDepartment(item: DepartmentUiData) {
+    this.router.navigate([`employment/departments/${item.id}`])
+  }
+
+  ngOnDestroy(): void {
+    this.loading = true
+  }
 }
