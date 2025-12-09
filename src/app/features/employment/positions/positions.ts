95fd44b2840b716896384864b1037e4a0d112412
yohagos <yosef.hagos@googlemail.com>
2025-12-09 21:47:42 +0100

    fix: loading and displaying positions

--- src/app/features/employment/positions/positions.ts
+++ src/app/features/employment/positions/positions.ts
@@ -1,11 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnDestroy } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { PositionService } from './services/position-service';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { toObservable } from '@angular/core/rxjs-interop';
+import { PositionCard } from './position-card/position-card';
 
 @Component({
   selector: 'app-positions',
-  imports: [],
+  imports: [
+    MatButtonModule,
+    MatProgressBarModule,
+
+    PositionCard,
+  ],
   templateUrl: './positions.html',
   styleUrl: './positions.scss',
 })
-export class Positions {
+export class Positions implements OnDestroy {
+  private positionService = inject(PositionService)
+  positions = this.positionService.positionsList
 
+  loading = true
+
+  constructor() {
+    toObservable(this.positions).subscribe(data => {
+      if (data) {
+        this.loading = false
+      }
+    })
+    this.positionService.loadPositionList()
+  }
+
+  ngOnDestroy(): void {
+    this.loading = true
+  }
 }
