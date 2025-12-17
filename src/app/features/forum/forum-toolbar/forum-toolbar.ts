15d70daa9cc664211302cecb405d99c1f5340b16
yohagos <yosef.hagos@googlemail.com>
2025-12-17 20:48:53 +0100

    feat: adding toolbar/drawer component specific for forum, to organize channels by user

---
+++ src/app/features/forum/forum-toolbar/forum-toolbar.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+@Component({
+  selector: 'app-forum-toolbar',
+  imports: [
+    MatButtonModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatToolbarModule,
+  ],
+  templateUrl: './forum-toolbar.html',
+  styleUrl: './forum-toolbar.scss',
+})
+export class ForumToolbar {
+
+  /*
+  TODO
+  -> Toolbar/Drawer for messages / channels
+  -> generally Public chat will be directly on the forum component
+  -> before entering, user needs to register/login with username and email
+  -> generated adapter and general service, need to implement the services
+  */
+
+}
