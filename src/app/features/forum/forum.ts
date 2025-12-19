397cf96c1d0cdfd15466d33e8fcba7790db207f5
yohagos <yosef.hagos@googlemail.com>
2025-12-19 17:23:28 +0100

    update: adding toolbar and router outlet for channels like public for everyone and direct messages between users

--- src/app/features/forum/forum.ts
+++ src/app/features/forum/forum.ts
@@ -1,11 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ForumToolbar } from './forum-toolbar/forum-toolbar';
+import { RouterOutlet } from '@angular/router';
+import { ForumLogin } from './forum-login/forum-login';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { ForumLoginService } from './services/forum-login-service';
 
 @Component({
   selector: 'app-forum',
-  imports: [],
+  imports: [
+    ForumToolbar,
+    RouterOutlet,
+
+    MatDialogModule,
+  ],
   templateUrl: './forum.html',
   styleUrl: './forum.scss',
 })
 export class Forum {
+  private dialog = inject(MatDialog)
+  private forumLoginService = inject(ForumLoginService)
 
+  constructor() {
+    const userData = this.forumLoginService.getCurrentForumUser()
+    if (!userData) {
+      this.openLoginDialog()
+    }
+  }
+
+  openLoginDialog() {
+    this.dialog.open(ForumLogin, {
+      width: "30em",
+    })
+  }
 }
