e2824c043163f69f4e4693185da91882fb8c945c
yohagos <yosef.hagos@googlemail.com>
2025-12-25 17:04:43 +0100

    fix: updating public channel component

--- src/app/features/forum/public-channel/public-channel.ts
+++ src/app/features/forum/public-channel/public-channel.ts
@@ -1,18 +1,25 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnDestroy, signal } from '@angular/core';
 import { ForumService } from '../services/forum-service';
 import { TextEditor } from '../text-editor/text-editor';
+import { Messages } from '../messages/messages';
+import { ForumLoginService } from '../services/forum-login-service';
+import { toObservable } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'app-public-channel',
   imports: [
     TextEditor,
+    Messages,
   ],
   templateUrl: './public-channel.html',
   styleUrl: './public-channel.scss',
 })
 export class PublicChannel {
   private forumService = inject(ForumService)
+  private forumLoginService = inject(ForumLoginService)
   publicMessages = this.forumService.messagesPublicChannel
+
+  loggedInUser = this.forumLoginService.forumUser
 
   constructor() {
     this.forumService.loadPublicChannelMessages()
