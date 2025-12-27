7cd25b60269d7a0c1d86036af4518a5df7739d27
yohagos <yosef.hagos@googlemail.com>
2025-12-27 18:19:02 +0100

    fix: updating selecting message as parent process and clean up scss

--- src/app/features/forum/public-channel/public-channel.ts
+++ src/app/features/forum/public-channel/public-channel.ts
@@ -4,6 +4,7 @@ import { TextEditor } from '../text-editor/text-editor';
 import { Messages } from '../messages/messages';
 import { ForumLoginService } from '../services/forum-login-service';
 import { toObservable } from '@angular/core/rxjs-interop';
+import { ForumMessageUi } from '../models/forum.models';
 
 @Component({
   selector: 'app-public-channel',
@@ -21,7 +22,19 @@ export class PublicChannel {
 
   loggedInUser = this.forumLoginService.forumUser
 
+  selectedParentMessageId: string | null = null
+
   constructor() {
     this.forumService.loadPublicChannelMessages()
+  }
+
+  markMessageAsParent(message: ForumMessageUi) {
+    if (this.selectedParentMessageId === message.id) {
+      this.selectedParentMessageId = null
+      this.forumService.markMessageAsParent(undefined)
+    } else {
+      this.selectedParentMessageId = message.id || null
+      this.forumService.markMessageAsParent(message)
+    }
   }
 }
